96c4bc9c9c2fd1c113634d5f26fd1f2252678418
shanhe <shanhel@qq.com>
2023-08-26 19:52:50 +0800

    构建基本项目

---
+++ src/main/java/com/newcodebbs/NewcodebbsApplication.java
@@ -0,0 +1,13 @@
+package com.newcodebbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcodebbsApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(NewcodebbsApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/newcodebbs/NewcodebbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newcodebbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewcodebbsApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
