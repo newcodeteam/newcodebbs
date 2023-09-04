bf0c7c79532a491e50ec5f5ae9d1ceba035af116
shanhe <shanhel@qq.com>
2023-09-04 18:55:48 +0800

    实体类和基本架构

---
+++ src/main/java/com/newcodebbs/controller/UserDataController.java
@@ -0,0 +1,20 @@
+package com.newcodebbs.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户表 前端控制器
+ * </p>
+ *
+ * @author shanhe
+ * @since 2023-09-04
+ */
+@RestController
+@RequestMapping("/user-data")
+public class UserDataController {
+
+}
