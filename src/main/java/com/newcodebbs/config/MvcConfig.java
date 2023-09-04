a921ba8e387f46cdf32f5a810530215ad25928a4
shanhe <shanhel@qq.com>
2023-09-04 19:38:07 +0800

    拦截器

--- src/main/java/com/newcodebbs/config/MvcConfig.java
+++ src/main/java/com/newcodebbs/config/MvcConfig.java
@@ -1,7 +1,13 @@
 package com.newcodebbs.config;
 
+import com.newcodebbs.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
 /**
  * <p>
  * 路由拦截器配置类
@@ -12,6 +18,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
     
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录拦截器
+        registry.addInterceptor(new LoginInterceptor())
+                .excludePathPatterns(
+                        "/shop/**",
+                        "/voucher/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/blog/hot",
+                        "/user/code",
+                        "/user/login"
+                ).order(1);
+        // token刷新的拦截器
+//        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
+    }
 }
